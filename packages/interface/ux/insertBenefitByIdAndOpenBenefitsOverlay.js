45c306963e91aff31e8924ee42693d347958a2f0
polux0 <afidelo@protonmail.com>
2023-10-31 13:44:03 +0100

    fix: up to date;

--- packages/interface/ux/insertBenefitByIdAndOpenBenefitsOverlay.js
+++ packages/interface/ux/insertBenefitByIdAndOpenBenefitsOverlay.js
@@ -73,6 +73,7 @@ const insertBenefitByIdAndOpenBenefitsOverlay = async function(content) {
 
             let wallet = localStorage.getItem("wallet");
             let existingOrder = await getOrderByWallet(wallet);
+            let orderUpdateOrPost;
             if(existingOrder){
                 console.log("existing order: ", existingOrder);
                 sendButton.textContent = "Update ➹";
@@ -201,18 +202,16 @@ const insertBenefitByIdAndOpenBenefitsOverlay = async function(content) {
             dl.href = downloadURL;
         }
 
-        if (localStorage.getItem('pbi') == false){
+        if (localStorage.getItem('pbi') == false || localStorage.getItem('alreadyReceivedPhysicalBook' == 'true')){
             console.log('pbi: ', localStorage.getItem('pbi'));
             modifyBenefits(); 
         }
         // enter the discourse button
     }
     let enterTheDiscourseButton = document.getElementById('enterTheDiscourseButton');
-    console.log('fetched enterTheDiscourseButton', enterTheDiscourseButton);
     if(enterTheDiscourseButton){
         enterTheDiscourseButton.addEventListener('click', function(){
-            console.log('Last step was clicked!');
-            window.open('https://discord.gg/phazawkuSq', '_blank').focus();
+            window.open('https://discord.gg/MWApYu2MR8', '_blank').focus();
         });
     }
 
